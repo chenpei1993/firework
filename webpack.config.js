d73512c876cba9b6e6856e2e98e5c5dab2c5fe3c
jens <1035788780@qq.com>
2024-08-17 17:23:07 +0800

    增加压缩和混淆代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HTMLWebpackPlugin = require("html-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -29,7 +30,27 @@ module.exports = {
                 template: "./src/index.html"
             }
         )
-    ]
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                // 可选的 TerserPlugin 配置
+                terserOptions: {
+                    compress: {
+                        drop_console: true, // 删除所有的 `console` 语句
+                        dead_code: true, // 移除未被引用的代码
+                    },
+                    mangle: {
+                        safari10: true, // 兼容 Safari 10
+                    },
+                    output: {
+                        comments: false, // 删除所有的注释
+                        beautify: false, // 最小化输出
+                    },
+                },
+            }),
+        ],
+    },
 }
 
 
